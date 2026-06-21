2f14cc54ea8abf252ffedcc6ef0f062eb87e3635
unknown <djaouadgharbi10@gmail.com>
2026-06-21 10:55:01 +0100

    added order item DTO

---
+++ src/application/dto/order-item.dto.ts
@@ -0,0 +1,15 @@
+import type { MoneyDTO } from "./money.dto.js";
+import type { WeightDTO } from "./weight.dto.js";
+
+export type OrderItemDTO = {
+  id: string;
+  variationId: string;
+  qty: number;
+  unitPriceAtOrderTime: MoneyDTO;
+  unitDiscountPriceAtOrderTime: MoneyDTO | null;
+  weightAtOrderTime: WeightDTO;
+  lineTotal: MoneyDTO;
+  discountAmount: MoneyDTO | null;
+  totalWeight: WeightDTO;
+  hasDiscount: boolean;
+};
