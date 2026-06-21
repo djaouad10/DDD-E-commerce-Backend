4ea1bdd5df4a9ae935a12d3118c5647fbe82ef3a
unknown <djaouadgharbi10@gmail.com>
2026-06-21 11:30:28 +0100

    updated Currency union type to enum

--- src/domain/value-objects/money.ts
+++ src/domain/value-objects/money.ts
@@ -2,7 +2,12 @@ import type { MoneyDTO } from "#/application/dto/money.dto.js";
 import { ValidationError } from "#/shared/errors/domain-error.js";
 
 // add more currencies here in the future
-export type Currency = "DZD";
+
+export const Currency = {
+  DZD: "DZD",
+} as const;
+
+export type Currency = (typeof Currency)[keyof typeof Currency];
 
 export class Money {
   private constructor(
@@ -10,18 +15,30 @@ export class Money {
     readonly currency: Currency,
   ) {}
 
-  static of(amount: number): Money {
+  static of(amount: number, currency: Currency): Money {
     if (amount < 0)
       throw new ValidationError("amount", "Money can not be negative");
 
-    return new Money(amount, "DZD");
+    return new Money(amount, currency);
   }
 
   add(other: Money): Money {
-    return new Money(this.amount + other.amount, "DZD");
+    if (this.currency !== other.currency)
+      throw new ValidationError(
+        "currency",
+        "Money must have the same currency",
+      );
+
+    return new Money(this.amount + other.amount, this.currency);
   }
 
   subtract(other: Money): Money {
+    if (this.currency !== other.currency)
+      throw new ValidationError(
+        "currency",
+        "Money must have the same currency",
+      );
+
     if (this.amount < other.amount)
       throw new ValidationError("amount", "Money can not be negative");
 
@@ -29,7 +46,7 @@ export class Money {
   }
 
   multiply(qty: number): Money {
-    return new Money(this.amount * qty, "DZD");
+    return new Money(this.amount * qty, this.currency);
   }
 
   toDTO(): MoneyDTO {
