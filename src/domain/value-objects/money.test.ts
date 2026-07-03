2f6da5fbceaa7af2778fb9d990feb6ae7487ce01
unknown <djaouadgharbi10@gmail.com>
2026-07-04 00:33:28 +0100

    added unit test for Money.add()

--- src/domain/value-objects/money.test.ts
+++ src/domain/value-objects/money.test.ts
@@ -1,6 +1,6 @@
 // What to test:
-// 1. of()
-// 2. add()
+// DONE 1. of()
+// DONE 2. add()
 // 3. subtract()
 // 4. multiply()
 
@@ -28,6 +28,30 @@ describe("Money Value Object", () => {
     test("when amount is negative, it should throw a ValidationError", () => {
       // Arrange & Act & Assert
       expect(() => Money.of(-1, "DZD")).toThrow(ValidationError);
+    });
+  });
+
+  describe("Money.add()", () => {
+    test("when currencies are the same, it should return a sum Money instance with the same currency", () => {
+      // Arrange
+      const money1 = Money.of(100, "DZD");
+      const money2 = Money.of(100, "DZD");
+
+      // Act
+      const result = money1.add(money2);
+
+      // Assert
+      expect(result).toStrictEqual(Money.of(200, "DZD"));
+    });
+
+    test("when currencies are different, it should throw a ValidationError", () => {
+      // Arrange
+      const money1 = Money.of(100, "DZD");
+      // @ts-expect-error: I don't use USD, just to test the error
+      const money2 = Money.of(100, "USD");
+
+      // Act & Assert
+      expect(() => money1.add(money2)).toThrow(ValidationError);
     });
   });
 });
