e518e9002e722a7b8d72bed882092509524db980
unknown <djaouadgharbi10@gmail.com>
2026-07-05 08:11:29 +0100

    added VariationDTO

---
+++ src/application/dto/variation.dto.ts
@@ -0,0 +1,16 @@
+import type { Color, Size } from "#/domain/entities/product.js";
+import type { WeightDTO } from "./weight.dto.js";
+
+export type VariationDTO = {
+  id: string;
+  productId: string;
+  size: Size;
+  color: Color;
+  totalQty: number;
+  reservedQty: number;
+  availableQty: number;
+  isInStock: boolean;
+  weightInGrams: WeightDTO;
+  createdAt: string;
+  updatedAt: string;
+};
