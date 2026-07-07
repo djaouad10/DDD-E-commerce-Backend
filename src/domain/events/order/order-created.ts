5923660d58d305beda4c7d8a8edde549e13acf2e
unknown <djaouadgharbi10@gmail.com>
2026-07-07 14:13:23 +0100

    added order.created domain event

---
+++ src/domain/events/order/order-created.ts
@@ -0,0 +1,14 @@
+import type { DomainEvent } from "../domain-event.js";
+
+export class OrderCreated implements DomainEvent {
+  readonly eventType = "order.created";
+  readonly occurredOn: Date = new Date();
+
+  constructor(
+    readonly aggregateId: string,
+    readonly userId: string,
+    readonly itemCount: number,
+    readonly totalPrice: number,
+    readonly currency: string,
+  ) {}
+}
