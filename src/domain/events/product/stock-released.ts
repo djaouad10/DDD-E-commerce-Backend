bb1e47503ce8edec94e22dda8a9f18fa6c89f527
unknown <djaouadgharbi10@gmail.com>
2026-07-08 05:46:35 +0100

    added stock released domain event

---
+++ src/domain/events/product/stock-released.ts
@@ -0,0 +1,13 @@
+import type { DomainEvent } from "../domain-event.js";
+
+export class StockReleased implements DomainEvent {
+  readonly eventType = "stock.released";
+  readonly occurredOn = new Date();
+
+  constructor(
+    readonly aggregateId: string,
+    readonly productId: string,
+    readonly qty: number,
+    readonly newAvailableQty: number,
+  ) {}
+}
