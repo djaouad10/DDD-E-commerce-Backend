e1b4cc4f5928b70d7f6fdc6ac708889114b57b07
unknown <djaouadgharbi10@gmail.com>
2026-07-08 05:42:03 +0100

    added product image removed domain event

---
+++ src/domain/events/product/product-image-removed.ts
@@ -0,0 +1,11 @@
+import type { DomainEvent } from "../domain-event.js";
+
+export class ProductImageRemoved implements DomainEvent {
+  readonly eventType = "product.image-removed";
+  readonly occurredOn = new Date();
+
+  constructor(
+    readonly aggregateId: string,
+    readonly imageId: string,
+  ) {}
+}
