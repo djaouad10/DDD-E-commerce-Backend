241f145ddfd3801b3c27c1245663f50ea9b51f50
unknown <djaouadgharbi10@gmail.com>
2026-07-14 10:48:31 +0100

    added TrackingHistory value object

---
+++ src/domain/value-objects/tracking-history.ts
@@ -0,0 +1,25 @@
+export const OrderTrackingStatus = {
+  ORDER_INFORMATION_RECEIVED_BY_CARRIER:
+    "order_information_received_by_carrier",
+  PICKED: "picked",
+  DISPATCHED_TO_DRIVER: "dispatched_to_driver",
+  ATTEMPT_DELIVERY: "attempt_delivery",
+  RETURN_ASKED: "return_asked",
+  RETURN_IN_TRANSIT: "return_in_transit",
+  RETURN_RECEIVED: "Return_received",
+  SUSPENDED: "suspended",
+  DELIVERED: "delivered",
+  PAID: "paid",
+} as const;
+
+export type OrderTrackingStatus =
+  (typeof OrderTrackingStatus)[keyof typeof OrderTrackingStatus];
+
+export class TrackingHistory {
+  constructor(
+    date: Date,
+    time: string,
+    status: OrderTrackingStatus,
+    station?: string,
+  ) {}
+}
