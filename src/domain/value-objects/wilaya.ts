fc7346e1d0a54e7d1f683360649276e97c2076a0
unknown <djaouadgharbi10@gmail.com>
2026-07-14 10:34:30 +0100

    made wilaya value object constructor public

--- src/domain/value-objects/wilaya.ts
+++ src/domain/value-objects/wilaya.ts
@@ -1,14 +1,3 @@
 export class Wilaya {
-  private constructor(
-    public readonly code: number,
-    public readonly name: string,
-  ) {}
-
-  static create(code: number, name: string): Wilaya {
-    return new Wilaya(code, name);
-  }
-
-  static createMany(params: { code: number; name: string }[]): Wilaya[] {
-    return params.map((param) => new Wilaya(param.code, param.name));
-  }
+  constructor(code: number, name: string) {}
 }
