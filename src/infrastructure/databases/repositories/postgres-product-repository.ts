1a90da0ddbfa9310c8b6e95730cd480dc28d2b88
unknown <djaouadgharbi10@gmail.com>
2026-07-16 09:12:33 +0100

    implemented PostgresProductRepository.findBySlug method

--- src/infrastructure/databases/repositories/postgres-product-repository.ts
+++ src/infrastructure/databases/repositories/postgres-product-repository.ts
@@ -9,6 +9,7 @@ import {
   type ProductWithVariationsAndFilesRow,
 } from "../mappers/postgres-product-mapper.js";
 import { product, rating } from "../schema.js";
+import type { Slug } from "#/domain/value-objects/slug.js";
 
 export class PostgresProductRepository implements ProductRepository {
   constructor(private db: DrizzleDBClient) {}
@@ -47,6 +48,45 @@ export class PostgresProductRepository implements ProductRepository {
       throw new DatabaseError(
         error instanceof Error ? error.message : "Unknown database error",
         "PostgresProductRepository.find",
+        error,
+      );
+    }
+  }
+
+  async findBySlug(slug: Slug): Promise<Product | null> {
+    try {
+      // get product with variations and files
+      const productWithVariationsAndFilesRow:
+        | ProductWithVariationsAndFilesRow
+        | undefined = await this.db.query.product.findFirst({
+        where: eq(product.slug, slug.value),
+        with: { variations: true, images: true },
+      });
+
+      // if product not found return null
+      if (!productWithVariationsAndFilesRow) {
+        return null;
+      }
+
+      // get average rating of product
+      const [ratingResult] = await this.db
+        .select({
+          productId: rating.product_id,
+          averageRating: avg(rating.rating).mapWith(Number),
+        })
+        .from(rating)
+        .where(eq(rating.product_id, productWithVariationsAndFilesRow.id))
+        .groupBy(rating.product_id);
+
+      // reconstitute product aggregate and return it
+      return PostgresProductMapper.toDomain(
+        productWithVariationsAndFilesRow,
+        ratingResult?.averageRating ?? null,
+      );
+    } catch (error) {
+      throw new DatabaseError(
+        error instanceof Error ? error.message : "Unknown database error",
+        "PostgresProductRepository.findBySlug",
         error,
       );
     }
