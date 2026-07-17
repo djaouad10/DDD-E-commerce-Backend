e7ac12088776fd7418b5626280f4659354617643
unknown <djaouadgharbi10@gmail.com>
2026-07-17 11:35:25 +0100

    implemented PostgresCartMapper.toRows

--- src/infrastructure/databases/mappers/postgres-cart-mapper.ts
+++ src/infrastructure/databases/mappers/postgres-cart-mapper.ts
@@ -21,4 +21,15 @@ export class PostgresCartMapper {
 
     return Cart.reconstitute(CartId.generate(), UserId.of(userId), cartItems);
   }
+
+  static toRows(cart: Cart): CartItemRow[] {
+    return cart.getItems().map((item) => ({
+      id: item.id.value,
+      user_id: cart.userId.value,
+      updated_at: item.getUpdatedAt(),
+      created_at: item.getUpdatedAt(), // created_at is not relevant in our domain, so just fill it with whatever
+      variation_id: item.variationId.value,
+      selected_qty: item.getQty(),
+    }));
+  }
 }
