c5997f96252ecbb659ca08e1efca91b2f8fbbb73
unknown <djaouadgharbi10@gmail.com>
2026-07-17 17:17:34 +0100

    added an optional transaction client param to OutboxRepository methods

--- src/application/repositories/outbox.repository.ts
+++ src/application/repositories/outbox.repository.ts
@@ -1,7 +1,7 @@
-// the OutboxRepository is defined here(application layer) instead of domain layer because the interface exists only to support application workflows or technical patterns
-
 import type { DomainEvent } from "#/domain/events/domain-event.js";
+import type { TransactionClient } from "#/shared/types/transaction-client.js";
 
+// the OutboxRepository is defined here(application layer) instead of domain layer because the interface exists only to support application workflows or technical patterns
 // if an interface accepts or returns domain entities/aggregates, it belongs to the domain layer, which is not the case here
 
 export const OutboxAction = {
@@ -16,12 +16,15 @@ export type OutboxAction = (typeof OutboxAction)[keyof typeof OutboxAction];
 
 export interface OutboxRepository {
   // called by application services inside the same transaction as aggregate saves
-  saveJob(params: {
-    action: OutboxAction;
-    payload: Record<string, unknown>;
-    scheduledAt?: Date; // optional, defaults to now
-  }): Promise<void>;
+  saveJob(
+    params: {
+      action: OutboxAction;
+      payload: Record<string, unknown>;
+      scheduledAt?: Date; // optional, defaults to now
+    },
+    tx?: TransactionClient,
+  ): Promise<void>;
 
-  saveEvents(events: DomainEvent[]): Promise<void>;
+  saveEvents(events: DomainEvent[], tx?: TransactionClient): Promise<void>;
   // the actual implementation will contain more methods like: getPendingJobs, getPendingEvents, update,... etc. but these are used by workers(infrastructure layer), the application layer is not concerned with them
 }
