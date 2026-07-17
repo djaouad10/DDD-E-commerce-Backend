4138afaccf9d47babadbdc9877dcf76c294a0d66
unknown <djaouadgharbi10@gmail.com>
2026-07-17 16:38:57 +0100

    added outbox repository definition in application layer

---
+++ src/application/repositories/outbox.repository.ts
@@ -0,0 +1,28 @@
+// the OutboxRepository is defined here(application layer) instead of domain layer because the interface exists only to support application workflows or technical patterns
+
+import type { DomainEvent } from "#/domain/events/domain-event.js";
+
+// if an interface accepts or returns domain entities/aggregates, it belongs to the domain layer, which is not the case here
+
+export const OutboxAction = {
+  CREATE_ORDER_IN_SHIPPING_API: "create_order_in_shipping_api",
+  DELETE_ORDER_IN_SHIPPING_API: "delete_order_in_shipping_api",
+  UPDATE_ORDER_IN_SHIPPING_API: "update_order_in_shipping_api",
+  CREATE_SHIPMENT_SHIPPING_IN_SHIPPING_API:
+    "create_shipment_shipping_in_shipping_api",
+} as const;
+
+export type OutboxAction = (typeof OutboxAction)[keyof typeof OutboxAction];
+
+export interface OutboxRepository {
+  // called by application services inside the same transaction as aggregate saves
+  saveJob(params: {
+    id: string;
+    action: OutboxAction;
+    payload: Record<string, unknown>;
+    scheduledAt?: Date; // optional, defaults to now
+  }): Promise<void>;
+
+  saveEvents(events: DomainEvent[]): Promise<void>;
+  // the actual implementation will contain more methods like: getPendingJobs, getPendingEvents, update,... etc. but these are used by workers(infrastructure layer), the application layer is not concerned with them
+}
