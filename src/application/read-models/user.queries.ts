e05a6757b32637965fe88e4a007fc0cb215dcefb
unknown <djaouadgharbi10@gmail.com>
2026-07-19 11:35:43 +0100

    added cursor to UserSearchCriteria

--- src/application/read-models/user.queries.ts
+++ src/application/read-models/user.queries.ts
@@ -1,11 +1,15 @@
 import type { UserRole } from "#/domain/entities/user.js";
+import type { UserId } from "#/domain/value-objects/user-id.js";
 import type { UserDTO } from "../dto/user.dto.js";
 
 export type UserSearchCriteria = {
   limit: number;
   role: UserRole;
+  cursor?: UserId;
 };
 
 export type UserQueries = {
-  search: (criteria: UserSearchCriteria) => Promise<UserDTO[]>;
+  search: (
+    criteria: UserSearchCriteria,
+  ) => Promise<{ users: UserDTO[]; nextCursor?: string }>;
 };
