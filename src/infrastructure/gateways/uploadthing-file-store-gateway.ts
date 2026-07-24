cefd2e786509209c14f1167da65f6f6617755380
unknown <djaouadgharbi10@gmail.com>
2026-07-24 15:21:09 +0100

    implemented UploadthingFileStoreGateway.deleteMany and .getPublicFileUrl

--- src/infrastructure/gateways/uploadthing-file-store-gateway.ts
+++ src/infrastructure/gateways/uploadthing-file-store-gateway.ts
@@ -3,6 +3,7 @@ import { GatewayError } from "#/shared/errors/domain-error.js";
 import { handleUploadThingErrors } from "#/shared/errors/handle-uploadthing-errors.js";
 import { createLogger } from "#/shared/logging/logger.js";
 import { UTApi } from "uploadthing/server";
+import { env } from "../config/env.js";
 
 export class UploadthingFileStoreGateway implements FileStoreGateway {
   private logger = createLogger("UploadthingFileStoreGateway");
@@ -23,5 +24,35 @@ export class UploadthingFileStoreGateway implements FileStoreGateway {
 
       handleUploadThingErrors(error, "UploadthingFileStoreGateway.delete");
     }
+  }
+
+  async deleteMany(keys: string[]): Promise<void> {
+    this.logger.debug(`deleteMany called`, { keys });
+    try {
+      const res = await this.utApi.deleteFiles(keys);
+
+      if (!res.success)
+        throw new GatewayError("UploadthingFileStoreGateway", undefined);
+
+      if (res.deletedCount !== keys.length) {
+        this.logger.warn(`deleteMany failed to delete all files`, { keys });
+      }
+
+      this.logger.debug(`delete completed`, { keys });
+    } catch (error) {
+      this.logger.error(`delete failed`, error as Error, { keys });
+
+      handleUploadThingErrors(error, "UploadthingFileStoreGateway.deleteMany");
+    }
+  }
+
+  getPublicFileUrl(key: string): string {
+    this.logger.debug(`getPublicUrl called`, { key });
+
+    const url = `https://${env.UPLOADTHING_APP_ID}.ufs.sh/f/${key}`;
+
+    this.logger.debug(`getPublicUrl completed`, { key, url });
+
+    return url;
   }
 }
