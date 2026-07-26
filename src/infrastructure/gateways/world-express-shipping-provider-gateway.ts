62aca521c4bb40973af06d7899eda304b182d5ac
unknown <djaouadgharbi10@gmail.com>
2026-07-26 16:34:00 +0100

    implemented WorldExpressShippingProviderGateway.createManyShipments and .formatManyShipmentsResBody

--- src/infrastructure/gateways/world-express-shipping-provider-gateway.ts
+++ src/infrastructure/gateways/world-express-shipping-provider-gateway.ts
@@ -1,5 +1,6 @@
 import type { Order } from "#/domain/entities/order.js";
 import type { ShippingProviderGateway } from "#/domain/gateways/shipping-provider.gateway.js";
+import { OrderId } from "#/domain/value-objects/order-id.js";
 import {
   HttpConnectionError,
   HttpTimeoutError,
@@ -21,7 +22,7 @@ export class WorldExpressShippingProviderGateway implements ShippingProviderGate
   ) {}
 
   async createShipment(order: Order): Promise<{ trackingNumber: string }> {
-    this.logger.info("createShipment called");
+    this.logger.debug("createShipment called");
 
     const orderId = order.id;
     const shippingDetails = order.getShippingDetails();
@@ -64,6 +65,9 @@ export class WorldExpressShippingProviderGateway implements ShippingProviderGate
         throw new WorldExpressApiError(response.statusCode, response.body, url);
       }
 
+      this.logger.debug("createShipment completed", {
+        trackingNumber: response.body.tracking,
+      });
       return {
         trackingNumber: response.body.tracking,
       };
@@ -84,8 +88,110 @@ export class WorldExpressShippingProviderGateway implements ShippingProviderGate
     }
   }
 
+  async createManyShipments(orders: Order[]): Promise<{
+    failed: OrderId[];
+    created: { orderId: OrderId; trackingNumber: string }[];
+  }> {
+    this.logger.debug("createManyShipments called");
+
+    try {
+      const reqBody = orders.map((order) => {
+        const orderId = order.id;
+        const shippingDetails = order.getShippingDetails();
+
+        return {
+          reference: orderId.value,
+          nom_client: shippingDetails.getFullName(),
+          telephone: shippingDetails.getFirstPhone(),
+          telephone_2: shippingDetails.getSecondPhone() ?? "",
+          adresse: shippingDetails.getAddress(),
+          code_postal: shippingDetails.getPostalCode(),
+          commune: shippingDetails.getCommune(),
+          code_wilaya: String(shippingDetails.getWilayaCode()),
+          montant: String(order.getTotalOrderPrice().amount),
+          remarque: shippingDetails.getClientNote() ?? "",
+          stock: "0",
+          type: "1",
+          stop_desk: shippingDetails.deliveryType === "TO_DESK" ? "1" : "0",
+          weight: String(order.getTotalWeightInKg().weight),
+          fragile: shippingDetails.getFragile() ? "1" : "0",
+          gps_link: shippingDetails.getGpsLink() ?? "",
+        };
+      });
+
+      const url = `${this.baseUrl}/create/orders`;
+
+      const response = await this.logger.measure(
+        `httpClient.request(${url})`,
+        () =>
+          this.httpClient.request<WECreateManyShipmentsResBody>({
+            url,
+            method: "POST",
+            headers: {
+              Authorization: `Bearer ${this.apiKey}`,
+            },
+            body: reqBody, // serilized by the http client
+          }),
+      );
+
+      if (!this.isSuccess(response.statusCode)) {
+        throw new WorldExpressApiError(response.statusCode, response.body, url);
+      }
+
+      const { created, failed } = this.formatManyShipmentsResBody(
+        response.body,
+      );
+
+      this.logger.debug("createManyShipments completed", {
+        createdCount: created.length,
+        failedCount: failed.length,
+      });
+
+      return { created, failed };
+    } catch (error) {
+      this.logger.error("createManyShipments failed", error as Error);
+
+      if (
+        error instanceof HttpTimeoutError ||
+        error instanceof HttpConnectionError
+      ) {
+        throw error;
+      }
+
+      handleWorldExpressErrors(
+        error,
+        "WorldExpressShippingProviderGateway.createManyShipments",
+      );
+    }
+  }
+
   private isSuccess(status: number): boolean {
     return status >= 200 && status < 300;
+  }
+
+  private formatManyShipmentsResBody(body: WECreateManyShipmentsResBody): {
+    failed: OrderId[];
+    created: { orderId: OrderId; trackingNumber: string }[];
+  } {
+    const { results } = body;
+
+    const created: { orderId: OrderId; trackingNumber: string }[] = [];
+    const failed: OrderId[] = [];
+
+    for (const index in results) {
+      const result = results[index]!;
+
+      if (result.success) {
+        created.push({
+          orderId: OrderId.of(result.reference),
+          trackingNumber: result.tracking,
+        });
+      } else {
+        failed.push(OrderId.of(result.reference));
+      }
+    }
+
+    return { created, failed };
   }
 }
 
@@ -93,4 +199,15 @@ type WECreateShipmentResBody = {
   tracking: string;
   success: boolean;
   reference: string;
+};
+
+type WECreateManyShipmentsResBody = {
+  results: Record<
+    string,
+    {
+      success: boolean;
+      tracking: string;
+      reference: string;
+    }
+  >;
 };
