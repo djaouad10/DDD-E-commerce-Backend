9f6c06deb104e02eb9acd78d117ae8f4f7380983
unknown <djaouadgharbi10@gmail.com>
2026-07-28 17:21:08 +0100

    added Registration type for DI container

--- src/composition/container.ts
+++ src/composition/container.ts
@@ -1,4 +1,4 @@
-/** 
+/**
  * a Constructor represents any class that can be instantiated "new".
  ** the T generic means this constructor returns a T instance when called
  */
@@ -16,6 +16,17 @@ export type Token<T> = string | symbol | Constructor<T>;
  ** it returns and object instance of type T
  */
 export type Factory<T> = (scope: Scope) => T;
+
+
+/**
+ * a Registration is what we store about a specific dependancy.
+ ** it contains the factory (RECIPE) used to instantiate this dependency
+ ** it also contains the lifecycle rule of this dependency
+ */
+export type Registration<T> = {
+  factory: Factory<T>;
+  lifecycle: "singleton" | "scoped" | "transient";
+};
 
 export class Container {}
 
