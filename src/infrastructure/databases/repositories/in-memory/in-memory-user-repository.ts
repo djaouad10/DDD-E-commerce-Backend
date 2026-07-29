1109b2a8918e6a23ea4c162d5e3a7bc79c888407
unknown <djaouadgharbi10@gmail.com>
2026-07-29 16:45:46 +0100

    added in memory User repository

---
+++ src/infrastructure/databases/repositories/in-memory/in-memory-user-repository.ts
@@ -0,0 +1,17 @@
+import type { User } from "#/domain/entities/user.js";
+import type { UserRepository } from "#/domain/repositories/user.repository.js";
+import type { UserId } from "#/domain/value-objects/user-id.js";
+
+export class InMemoryUserRepository implements UserRepository {
+  private users: User[] = [];
+
+  async find(id: UserId): Promise<User | null> {
+    return this.users.find((user) => user.id.equals(id)) ?? null;
+  }
+  async findByEmail(email: string): Promise<User | null> {
+    return this.users.find((user) => user.email === email) ?? null;
+  }
+  async findMany(ids: UserId[]): Promise<User[]> {
+    return this.users.filter((user) => ids.some((id) => id.equals(user.id)));
+  }
+}
