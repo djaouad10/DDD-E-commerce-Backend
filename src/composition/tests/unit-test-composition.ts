c56c0cefff3d5ffc5b6ff99468eb8b46ee68ad52
unknown <djaouadgharbi10@gmail.com>
2026-07-30 09:21:33 +0100

    implemented unit tests composition root

---
+++ src/composition/tests/unit-test-composition.ts
@@ -0,0 +1,95 @@
+import { createDb } from "#/infrastructure/config/database.js";
+import { env } from "#/infrastructure/config/env.js";
+import { createRedisConnection } from "#/infrastructure/config/redis-connection.js";
+import { InMemoryCartRepository } from "#/infrastructure/databases/repositories/in-memory/in-memory-cart-repository.js";
+import { InMemoryCategoryRepository } from "#/infrastructure/databases/repositories/in-memory/in-memory-category-repository.js";
+import { InMemoryOrderRepository } from "#/infrastructure/databases/repositories/in-memory/in-memory-order-repository.js";
+import { InMemoryOutboxRepository } from "#/infrastructure/databases/repositories/in-memory/in-memory-outbox-repository.js";
+import { InMemoryProductRepository } from "#/infrastructure/databases/repositories/in-memory/in-memory-product-repository.js";
+import { InMemoryRatingRepository } from "#/infrastructure/databases/repositories/in-memory/in-memory-rating-repository.js";
+import { InMemoryUserRepository } from "#/infrastructure/databases/repositories/in-memory/in-memory-user-repository.js";
+import { InMemoryFileStoreGateway } from "#/infrastructure/gateways/in-memory-file-store-gateway.js";
+import { Container } from "../container.js";
+import {
+  DB,
+  CART_REPOSITORY,
+  USER_REPOSITORY,
+  ORDER_REPOSITORY,
+  CATEGORY_REPOSITORY,
+  PRODUCT_REPOSITORY,
+  RATING_REPOSITORY,
+  OUTBOX_REPOSITORY,
+  REDIS,
+  FILE_STORE_GATEWAY,
+} from "../tokens.js";
+
+export function buildUnitTestsContainer(): Container {
+  const container = new Container();
+
+  // singletons
+  const testDb = createDb({ connectionUrl: env.DATABASE_URL, maxPoolSize: 1 });
+  container.registerInstance(DB, testDb);
+
+  const testRedisConnection = createRedisConnection({
+    host: env.REDIS_HOST,
+    port: env.REDIS_PORT,
+    maxRetriesPerRequest: null, // required by bullmq
+    enableReadyCheck: false, // required by bullmq
+    lazyConnect: true, // connection is established on first use instead of on server startup (faster startup time)
+  });
+  container.registerInstance(REDIS, testRedisConnection);
+
+  // repositories
+  container.register(
+    CART_REPOSITORY,
+    () => new InMemoryCartRepository(),
+    "singleton",
+  );
+
+  container.register(
+    CATEGORY_REPOSITORY,
+    () => new InMemoryCategoryRepository(),
+    "singleton",
+  );
+
+  container.register(
+    ORDER_REPOSITORY,
+    () => new InMemoryOrderRepository(),
+    "singleton",
+  );
+
+  container.register(
+    PRODUCT_REPOSITORY,
+    () => new InMemoryProductRepository(),
+    "singleton",
+  );
+
+  container.register(
+    RATING_REPOSITORY,
+    () => new InMemoryRatingRepository(),
+    "singleton",
+  );
+
+  container.register(
+    USER_REPOSITORY,
+    () => new InMemoryUserRepository(),
+    "singleton",
+  );
+
+  container.register(
+    OUTBOX_REPOSITORY,
+    () => new InMemoryOutboxRepository(),
+    "singleton",
+  );
+
+  // gateways
+  container.register(
+    FILE_STORE_GATEWAY,
+    () => new InMemoryFileStoreGateway(),
+    "singleton",
+  );
+
+  // services (scoped)
+
+  return container;
+}
