cc41eb715b114e95de140fa47190f0deabb7e6ac
unknown <djaouadgharbi10@gmail.com>
2026-07-30 08:09:48 +0100

    refactored in memory file store gateway

--- src/infrastructure/gateways/in-memory-file-store-gateway.ts
+++ src/infrastructure/gateways/in-memory-file-store-gateway.ts
@@ -1,18 +1,15 @@
 import type { FileStoreGateway } from "#/domain/gateways/file-store.gateway.js";
 
 export class InMemoryFileStoreGateway implements FileStoreGateway {
-  async delete(key: string): Promise<void> {}
+  async delete(): Promise<void> {}
 
-  async deleteMany(keys: string[]): Promise<void> {}
+  async deleteMany(): Promise<void> {}
 
-  async getPrivateFileReadUrl(
-    key: string,
-    expiresInSeconds?: number,
-  ): Promise<string> {
+  async getPrivateFileReadUrl(): Promise<string> {
     return "https://example.com/file/private/blablabla";
   }
 
-  getPublicFileUrl(key: string): string {
+  getPublicFileUrl(): string {
     return "https://example.com/file/public/blablabla";
   }
 }
