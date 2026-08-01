7c594603d35b67756ba89fa9e0cc221fc5d3bbbb
unknown <djaouadgharbi10@gmail.com>
2026-08-01 09:56:39 +0100

    added domain events processor composition root

--- src/composition/domain-events-processor-composition.ts
+++ src/composition/domain-events-processor-composition.ts
@@ -1,6 +1,24 @@
+import { PostgresOutboxRepository } from "#/infrastructure/databases/repositories/postgres/postgres-outbox-repository.js";
+import { createBullMqAnalyticsQueue } from "#/infrastructure/messaging/queue/analytics.queue.js";
+import { createBullMqEmailQueue } from "#/infrastructure/messaging/queue/email.queue.js";
+import { createBullMqInventoryQueue } from "#/infrastructure/messaging/queue/inventory.queue.js";
+import { FlowProducer } from "bullmq";
 import { Container } from "./container.js";
 import { registerSharedInfrastructure } from "./shared-registry.js";
-import { DB, REDIS } from "./tokens.js";
+import {
+  ANALYTICS_QUEUE,
+  BULLMQ_FLOW_PRODUCER,
+  DB,
+  DOMAIN_EVENTS_PROCESSOR_SERVICE,
+  EMAIL_QUEUE,
+  EVENT_PUBLISHER,
+  INVENTORY_QUEUE,
+  OUTBOX_REPOSITORY,
+  REDIS,
+} from "./tokens.js";
+import { createBullMqFlowProducer } from "#/infrastructure/messaging/bullmq/utils/bullmq-flow-producer.js";
+import { BullMqEventPublisher } from "#/infrastructure/messaging/bullmq/bullmq-event-publisher.js";
+import { DomainEventsProcessorService } from "#/application/services/domain-events-processor.service.js";
 
 export function buildDomainEventsProcessorContainer(): Container {
   const container = new Container();
@@ -8,8 +26,38 @@ export function buildDomainEventsProcessorContainer(): Container {
   registerSharedInfrastructure(container);
 
   container.register(
-    OUTBOX_QUEUE,
-    (scope) => createBullMqOutboxQueue(scope.resolve(REDIS)),
+    EMAIL_QUEUE,
+    (scope) => createBullMqEmailQueue(scope.resolve(REDIS)),
+    "singleton",
+  );
+
+  container.register(
+    ANALYTICS_QUEUE,
+    (scope) => createBullMqAnalyticsQueue(scope.resolve(REDIS)),
+    "singleton",
+  );
+
+  container.register(
+    INVENTORY_QUEUE,
+    (scope) => createBullMqInventoryQueue(scope.resolve(REDIS)),
+    "singleton",
+  );
+
+  container.register(
+    FlowProducer,
+    (scope) => createBullMqFlowProducer(scope.resolve(REDIS)),
+    "singleton",
+  );
+
+  container.register(
+    EVENT_PUBLISHER,
+    (scope) =>
+      new BullMqEventPublisher(
+        scope.resolve(BULLMQ_FLOW_PRODUCER),
+        scope.resolve(EMAIL_QUEUE),
+        scope.resolve(ANALYTICS_QUEUE),
+        scope.resolve(INVENTORY_QUEUE),
+      ),
     "singleton",
   );
 
@@ -20,11 +68,11 @@ export function buildDomainEventsProcessorContainer(): Container {
   );
 
   container.register(
-    OUTBOX_PROCESSOR_SERVICE,
+    DOMAIN_EVENTS_PROCESSOR_SERVICE,
     (scope) =>
-      new OutboxProcessorService(
+      new DomainEventsProcessorService(
         scope.resolve(OUTBOX_REPOSITORY),
-        scope.resolve(OUTBOX_QUEUE),
+        scope.resolve(EVENT_PUBLISHER),
       ),
     "scoped",
   );
