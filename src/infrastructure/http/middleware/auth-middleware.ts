be33bbe288619edf13f1ddfa7c491c6a6a980bbd
unknown <djaouadgharbi10@gmail.com>
2026-08-03 16:36:47 +0100

    implemented auth middleware

---
+++ src/infrastructure/http/middleware/auth-middleware.ts
@@ -0,0 +1,19 @@
+import { AUTH } from "#/composition/tokens.js";
+import type { NextFunction, Request, Response } from "express";
+
+export async function AuthMiddleware(
+  req: Request,
+  _res: Response,
+  next: NextFunction,
+) {
+  // not try catch since error handled by error middleware
+  const auth = await req.scope.resolve(AUTH);
+
+  const session = await auth.api.getSession({ headers: req.header });
+
+  if (session) {
+    req.user = { id: session.user.id, role: session.user.role };
+  }
+
+  next();
+}
