fb7b4ae6c199195e16084c5a204b5a7073d15f0c
unknown <djaouadgharbi10@gmail.com>
2026-08-04 08:41:47 +0100

    create api server entry point

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { Container } from "./composition/container.js";
+import { env } from "./infrastructure/config/env.js";
+import { createServer } from "./infrastructure/http/server/index.js";
+
+async function bootstrap() {
+  const container = new Container();
+  const app = createServer(container);
+
+  const port = env.PORT || 3000;
+
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}
+
+bootstrap();
