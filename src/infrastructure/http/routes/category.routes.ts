7ecea2f16a82cfaba9a96009ce9726da2f4a0f4a
unknown <djaouadgharbi10@gmail.com>
2026-08-08 07:48:07 +0100

    implemnted POST /api/v1/categories with validation utility and schema

--- src/infrastructure/http/routes/category.routes.ts
+++ src/infrastructure/http/routes/category.routes.ts
@@ -1,5 +1,11 @@
-import { GET_CATEGORIES_SERVICE } from "#/composition/tokens.js";
+import { CreateCategoryCommand } from "#/application/commands/create-category.command.js";
+import {
+  CREATE_CATEGORY_SERVICE,
+  GET_CATEGORIES_SERVICE,
+} from "#/composition/tokens.js";
 import { Router } from "express";
+import { createCategoryBodySchema } from "../validators/categories.js";
+import { validate } from "../utils/validation.js";
 
 const router = Router();
 
@@ -8,6 +14,17 @@ router.get("/", async (req, res) => {
   const result = await service.execute();
 
   res.status(200).json(result);
+});
+
+router.post("/", async (req, res) => {
+  const safeBody = validate(createCategoryBodySchema, req.body);
+
+  const service = req.scope.resolve(CREATE_CATEGORY_SERVICE);
+  const command = new CreateCategoryCommand(safeBody.name);
+
+  const result = await service.execute(command);
+
+  res.status(201).json(result);
 });
 
 export default router;
