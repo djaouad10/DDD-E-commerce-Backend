eeacc7b6817e6aa40e6d3af1866a6778f67d5403
unknown <djaouadgharbi10@gmail.com>
2026-08-08 10:17:34 +0100

    implemnted GET /api/v1/cart endpoint

--- src/infrastructure/http/routes/cart.routes.ts
+++ src/infrastructure/http/routes/cart.routes.ts
@@ -1,5 +1,19 @@
 import { Router } from "express";
+import { authMiddleware } from "../middleware/auth-middleware.js";
+import { GET_USER_CART_SERVICE } from "#/composition/tokens.js";
+import { GetUserCartQuery } from "#/application/queries/get-user-cart.query.js";
 
 const router = Router();
+
+router.get("/", authMiddleware, async (req, res) => {
+  const userId = req.user!.id; // auth middleware ensures req.user is defined
+
+  const service = req.scope.resolve(GET_USER_CART_SERVICE);
+  const query = new GetUserCartQuery(userId);
+
+  const result = await service.execute(query);
+
+  res.status(200).json(result);
+});
 
 export default router;
