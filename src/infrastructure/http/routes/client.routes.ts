04d38d5b71b8984b9436ee4ac0c0b1c19f87b8e5
unknown <djaouadgharbi10@gmail.com>
2026-08-09 10:19:10 +0100

    implemented GET /api/v1/clients/ban-status/:id

--- src/infrastructure/http/routes/client.routes.ts
+++ src/infrastructure/http/routes/client.routes.ts
@@ -2,9 +2,15 @@ import { Router } from "express";
 import { authMiddleware } from "../middleware/auth-middleware.js";
 import { validate } from "../utils/validation.js";
 import { getClientProfileSearchParamsSchema } from "../validators/client.js";
-import { GET_CLIENT_PROFILE_SERVICE } from "#/composition/tokens.js";
+import {
+  GET_CLIENT_BAN_STATUS_SERVICE,
+  GET_CLIENT_PROFILE_SERVICE,
+} from "#/composition/tokens.js";
 import { GetClientProfileQuery } from "#/application/queries/get-client-profile.query.js";
 import type { UserDTO } from "#/application/dto/user.dto.js";
+import { deleteCartItemParamsSchema } from "../validators/cart.js";
+import { GetClientBanStatusQuery } from "#/application/queries/get-client-ban-status.query.js";
+import { adminMiddleware } from "../middleware/admin-middleware.js";
 
 const router = Router();
 
@@ -35,6 +41,17 @@ router.get("/profile", authMiddleware, async (req, res) => {
   const userDto: UserDTO = result;
 
   return res.status(200).json(userDto);
+});
+
+router.get("/ban-status/:id", adminMiddleware, async (req, res) => {
+  const safeParams = validate(deleteCartItemParamsSchema, req.params);
+
+  const service = req.scope.resolve(GET_CLIENT_BAN_STATUS_SERVICE);
+  const query = new GetClientBanStatusQuery(safeParams.id);
+
+  const result = await service.execute(query);
+
+  return res.status(200).json(result);
 });
 
 export default router;
--- src/infrastructure/http/validators/client.ts
+++ src/infrastructure/http/validators/client.ts
@@ -3,3 +3,7 @@ import z from "zod";
 export const getClientProfileSearchParamsSchema = z.object({
   id: z.string(),
 });
+
+export const deleteCartItemParamsSchema = z.object({
+  id: z.string(),
+});
