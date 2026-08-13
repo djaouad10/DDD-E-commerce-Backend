7b806f55c9995ab83e16d8753b56e3ce505f35d7
unknown <djaouadgharbi10@gmail.com>
2026-08-13 17:03:49 +0100

    implemented GET /api/v1/orders/client endpoint

--- src/infrastructure/http/routes/order.routes.ts
+++ src/infrastructure/http/routes/order.routes.ts
@@ -1,5 +1,48 @@
 import { Router } from "express";
+import { validate } from "../utils/validation.js";
+import { getOrdersOfClientSearchParamsSchema } from "../validators/orders.js";
+import { GET_ORDERS_OF_CLIENT_SERVICE } from "#/composition/tokens.js";
+import { GetOrdersOfClientQuery } from "#/application/queries/get-orders-of-client.query.js";
+import { authMiddleware } from "../middleware/auth-middleware.js";
 
 const router = Router();
+
+router.get("/client", authMiddleware, async (req, res) => {
+  const service = req.scope.resolve(GET_ORDERS_OF_CLIENT_SERVICE);
+
+  if (req.user!.role === "ADMIN") {
+    const safeSearchParams = validate(
+      getOrdersOfClientSearchParamsSchema,
+      req.query,
+    );
+
+    const query = new GetOrdersOfClientQuery(
+      safeSearchParams.clientId,
+      safeSearchParams.limit ?? 10,
+      safeSearchParams.cursor,
+      safeSearchParams.status,
+    );
+
+    const result = await service.execute(query);
+
+    return res.status(200).json(result);
+  }
+
+  const safeSearchParams = validate(
+    getOrdersOfClientSearchParamsSchema.omit({ clientId: true }), // clientId doesn't exist for clients, so we omit it
+    req.query,
+  );
+
+  const query = new GetOrdersOfClientQuery(
+    req.user!.id, // we use current user id for clients
+    safeSearchParams.limit ?? 10,
+    safeSearchParams.cursor,
+    safeSearchParams.status,
+  );
+
+  const result = await service.execute(query);
+
+  res.status(200).json(result);
+});
 
 export default router;
---
+++ src/infrastructure/http/validators/orders.ts
@@ -0,0 +1,14 @@
+import { OrderStatus } from "#/domain/entities/order.js";
+import z from "zod";
+
+export const getOrdersOfClientSearchParamsSchema = z.object({
+  clientId: z.string(),
+  limit: z.coerce.number().min(1).optional(),
+  status: z.enum(OrderStatus).optional(),
+  cursor: z
+    .object({
+      createdAt: z.iso.datetime().pipe(z.coerce.date()),
+      orderId: z.string(),
+    })
+    .optional(),
+});
