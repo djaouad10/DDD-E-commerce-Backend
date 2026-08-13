f819d77ce1eccc1cbdfb425ac3f0448606abbd8c
unknown <djaouadgharbi10@gmail.com>
2026-08-13 17:15:10 +0100

    added an orderFactory test helper

--- src/tests/helpers/domain-helpers.ts
+++ src/tests/helpers/domain-helpers.ts
@@ -6,6 +6,11 @@ import { File } from "#/domain/entities/file.js";
 import { Variation } from "#/domain/entities/variation.js";
 import { Weight } from "#/domain/value-objects/weight.js";
 import { Money } from "#/domain/value-objects/money.js";
+import { ShippingDetails } from "#/domain/value-objects/shipping-details.js";
+import { OrderItem } from "#/domain/entities/order-item.js";
+import { VariationId } from "#/domain/value-objects/variation-id.js";
+import { ShippingProvider, Order } from "#/domain/entities/order.js";
+import { UserId } from "#/domain/value-objects/user-id.js";
 
 export function productFactory({
   categoryId,
@@ -56,3 +61,60 @@ export function productFactory({
     null,
   );
 }
+
+const makeValidShippingDetails = (): ShippingDetails => {
+  const validAlgerianPhoneNumber = "0678876545";
+
+  return ShippingDetails.create(
+    "TO_DESK",
+    faker.person.fullName(),
+    validAlgerianPhoneNumber,
+    faker.number.int({ min: 1, max: 69 }),
+    faker.location.city(),
+    faker.location.zipCode("#####"),
+    faker.location.streetAddress(),
+    true,
+    validAlgerianPhoneNumber,
+    faker.internet.url(),
+    faker.lorem.paragraphs(),
+  );
+};
+
+const makeValidOrderItems = (): OrderItem[] => {
+  return [
+    OrderItem.create(
+      VariationId.generate(),
+      faker.number.int({ min: 1, max: 50 }),
+      Money.of(3000, "DZD"),
+      Weight.of(100, "g"),
+      Money.of(2900, "DZD"),
+    ),
+    OrderItem.create(
+      VariationId.generate(),
+      faker.number.int({ min: 1, max: 50 }),
+      Money.of(2000, "DZD"),
+      Weight.of(200, "g"),
+      null,
+    ),
+  ];
+};
+
+export const orderFactory = ({
+  shippingDetails,
+  orderItems,
+  shippingPriceAtOrderTime,
+  userId,
+}: {
+  shippingDetails?: ShippingDetails;
+  orderItems?: OrderItem[];
+  userId?: UserId;
+  shippingPriceAtOrderTime?: Money;
+}): Order => {
+  return Order.create(
+    userId ?? UserId.generate(),
+    shippingDetails ?? makeValidShippingDetails(),
+    orderItems ?? makeValidOrderItems(),
+    shippingPriceAtOrderTime ?? Money.of(400, "DZD"),
+    ShippingProvider.WORLD_EXPRESS,
+  );
+};
