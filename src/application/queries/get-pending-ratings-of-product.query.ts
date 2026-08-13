0c95ba5632ee6e15c19ba4b37f9c65e40d7d3867
unknown <djaouadgharbi10@gmail.com>
2026-08-13 09:39:23 +0100

    implemented get-pending-ratings-of-product query/service

---
+++ src/application/queries/get-pending-ratings-of-product.query.ts
@@ -0,0 +1,18 @@
+import { ValidationError } from "#/shared/errors/domain-error.js";
+import type { RatingCursor } from "../read-models/rating.queries.js";
+
+export class GetPendingRatingsOfProductQuery {
+  constructor(
+    public readonly productId: string,
+    public readonly limit: number,
+    public readonly cursor?: RatingCursor,
+  ) {
+    this.validate(limit);
+  }
+
+  private validate(limit: number) {
+    if (limit <= 0) {
+      throw new ValidationError("limit", "limit must be greater than 0");
+    }
+  }
+}
