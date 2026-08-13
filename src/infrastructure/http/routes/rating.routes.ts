79593f287e1d9024b23234c3676864f809cac326
unknown <djaouadgharbi10@gmail.com>
2026-08-13 09:43:51 +0100

    implemented GET /api/v1/ratings/pending/:productId endpoint

--- src/infrastructure/http/routes/rating.routes.ts
+++ src/infrastructure/http/routes/rating.routes.ts
@@ -2,10 +2,16 @@ import { Router } from "express";
 import {
   getApprovedRatingsOfProductParamsSchema,
   getApprovedRatingsOfProductSearchParamsSchema,
+  getPendingRatingsOfProductParamsSchema,
+  getPendingRatingsOfProductSearchParamsSchema,
 } from "../validators/ratings.js";
 import { validate } from "../utils/validation.js";
-import { GET_APPROVED_RATINGS_OF_PRODUCT_SERVICE } from "#/composition/tokens.js";
+import {
+  GET_APPROVED_RATINGS_OF_PRODUCT_SERVICE,
+  GET_PENDING_RATINGS_OF_PRODUCT_SERVICE,
+} from "#/composition/tokens.js";
 import { GetApprovedRatingsOfProductQuery } from "#/application/queries/get-approved-ratings-of-product.query.js";
+import { GetPendingRatingsOfProductQuery } from "#/application/queries/get-pending-ratings-of-product.query.js";
 
 const router = Router();
 
@@ -21,6 +27,34 @@ router.get("/approved/:productId", async (req, res) => {
 
   const service = req.scope.resolve(GET_APPROVED_RATINGS_OF_PRODUCT_SERVICE);
   const query = new GetApprovedRatingsOfProductQuery(
+    safeParams.productId,
+    safeSearchParams.limit ?? 10,
+    safeSearchParams.cursor
+      ? {
+          productId: safeParams.productId,
+          createdAt: safeSearchParams.cursor.createdAt,
+          userId: safeSearchParams.cursor.userId,
+        }
+      : undefined,
+  );
+
+  const result = await service.execute(query);
+
+  res.status(200).json(result);
+});
+
+router.get("/pending/:productId", async (req, res) => {
+  const safeParams = validate(
+    getPendingRatingsOfProductParamsSchema,
+    req.params,
+  );
+  const safeSearchParams = validate(
+    getPendingRatingsOfProductSearchParamsSchema,
+    req.query,
+  );
+
+  const service = req.scope.resolve(GET_PENDING_RATINGS_OF_PRODUCT_SERVICE);
+  const query = new GetPendingRatingsOfProductQuery(
     safeParams.productId,
     safeSearchParams.limit ?? 10,
     safeSearchParams.cursor
--- src/infrastructure/http/validators/ratings.ts
+++ src/infrastructure/http/validators/ratings.ts
@@ -13,3 +13,18 @@ export const getApprovedRatingsOfProductSearchParamsSchema = z.object({
     })
     .optional(),
 });
+
+
+export const getPendingRatingsOfProductParamsSchema = z.object({
+  productId: z.string(),
+});
+
+export const getPendingRatingsOfProductSearchParamsSchema = z.object({
+  limit: z.coerce.number().min(1).optional(),
+  cursor: z
+    .object({
+      createdAt: z.iso.datetime().pipe(z.coerce.date()),
+      userId: z.string(),
+    })
+    .optional(),
+});
