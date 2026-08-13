5fea3b1972392e27ad36ff9adb41fa5f5a1d70a5
unknown <djaouadgharbi10@gmail.com>
2026-08-13 16:22:59 +0100

    refactored get-delivery-fees-of-wilaya service

--- src/application/services/get-delivery-fees-of-wilaya.service.ts
+++ src/application/services/get-delivery-fees-of-wilaya.service.ts
@@ -1,5 +1,5 @@
+import type { MoneySnapshot } from "#/domain/entities-snapshots/money.snapshot.js";
 import type { ShippingProviderGateway } from "#/domain/gateways/shipping-provider.gateway.js";
-import type { DeliveryFees } from "#/domain/value-objects/delivery-fees.js";
 import { createLogger } from "#/shared/logging/logger.js";
 import type { GetDeliveryFeesOfWilayaQuery } from "../queries/get-delivery-fees-of-wilaya.query.js";
 
@@ -8,7 +8,10 @@ export class GetDeliveryFeesOfWilayaService {
 
   constructor(private shippingProviderGateway: ShippingProviderGateway) {}
 
-  async execute(query: GetDeliveryFeesOfWilayaQuery): Promise<DeliveryFees> {
+  async execute(query: GetDeliveryFeesOfWilayaQuery): Promise<{
+    homeFees: MoneySnapshot;
+    deskFees: MoneySnapshot;
+  }> {
     this.logger.info("GetDeliveryFeesOfWilayaService.execute called", {
       provider: query.provider,
       wilayaCode: query.wilayaCode,
@@ -16,8 +19,13 @@ export class GetDeliveryFeesOfWilayaService {
 
     // here u would usually have an abstraction layer that maps the query.provider to the right shipping gateway implementation, but since I only have one shipping provider, I'll just use the gateway directly (it will use the only provider gateway implementation registered in API composition root, WORLD_EXPRESS_SHIPPING_PROVIDER_GATEWAY)
 
-    return await this.shippingProviderGateway.getDeliveryFeesOfWilaya(
+    const fees = await this.shippingProviderGateway.getDeliveryFeesOfWilaya(
       query.wilayaCode,
     );
+
+    return {
+      homeFees: fees.homeDeliveryFee.toSnapshot(),
+      deskFees: fees.stopDeskFee.toSnapshot(),
+    };
   }
 }
