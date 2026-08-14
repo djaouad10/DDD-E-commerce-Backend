2d0ee6790b28499289bad29b7a9d659c200725cd
unknown <djaouadgharbi10@gmail.com>
2026-08-14 21:10:00 +0100

    implemented get-orders query/service

--- src/application/queries/get-orders.query.ts
+++ src/application/queries/get-orders.query.ts
@@ -2,12 +2,11 @@ import type { OrderStatus } from "#/domain/entities/order.js";
 import { ValidationError } from "#/shared/errors/domain-error.js";
 import type { OrderCursor } from "../read-models/order.queries.js";
 
-export class GetOrdersOfClientQuery {
+export class GetOrdersQuery {
   constructor(
-    public readonly clientId: string,
     public readonly limit: number,
-    public readonly cursor?: OrderCursor,
     public readonly status?: OrderStatus,
+    public readonly cursor?: OrderCursor,
   ) {
     this.validate(limit);
   }
