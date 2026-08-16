0105a16b416911427be45bfc220d9870978ca55c
unknown <djaouadgharbi10@gmail.com>
2026-08-16 10:52:24 +0100

    refactored get-shipping-label-query

--- src/application/queries/get-shipping-label.query.ts
+++ src/application/queries/get-shipping-label.query.ts
@@ -1,3 +1,8 @@
+import type { ShippingProvider } from "#/domain/entities/order.js";
+
 export class GetShippingLabelQuery {
-  constructor(public readonly trackingNumber: string) {}
+  constructor(
+    public readonly trackingNumber: string,
+    public readonly provider: ShippingProvider,
+  ) {}
 }
