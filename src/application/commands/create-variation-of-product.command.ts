796bc03648b45188b843b3df29ba40fa3b8fb3de
unknown <djaouadgharbi10@gmail.com>
2026-08-18 14:47:46 +0100

    implemented createe-variation-of-product command/service

---
+++ src/application/commands/create-variation-of-product.command.ts
@@ -0,0 +1,30 @@
+import type { Color, Size } from "#/domain/entities/product.js";
+import { ValidationError } from "#/shared/errors/domain-error.js";
+
+export class CreateVariationOfProductCommand {
+  constructor(
+    public readonly productId: string,
+    public readonly data: {
+      size: Size;
+      color: Color;
+      totalQty: number;
+      weightInGrams: number;
+    },
+  ) {
+    this.validate(this.data.totalQty, this.data.weightInGrams);
+  }
+
+  private validate(totalQty: number, weightInGrams: number) {
+    if (totalQty < 0)
+      throw new ValidationError(
+        "product.variation.totalQty",
+        "must be greater than 0",
+      );
+
+    if (weightInGrams <= 0)
+      throw new ValidationError(
+        "product.variation.weightInGrams",
+        "must be greater than 0",
+      );
+  }
+}
