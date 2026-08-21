f55b42970b21589996256c47db9734542d323792
unknown <djaouadgharbi10@gmail.com>
2026-08-21 21:29:21 +0100

    implemented delete-product command/service

---
+++ src/application/services/delete-product.service.ts
@@ -0,0 +1,53 @@
+import type { ProductRepository } from "#/domain/repositories/product.repository.js";
+import { ProductId } from "#/domain/value-objects/product-id.js";
+import type { DrizzleDBClient } from "#/infrastructure/config/database.js";
+import { ConflictError, NotFoundError } from "#/shared/errors/domain-error.js";
+import { createLogger } from "#/shared/logging/logger.js";
+import type { DeleteProductCommand } from "../commands/delete-product.command.js";
+import type { OrderQueries } from "../read-models/order.queries.js";
+import type { OutboxRepository } from "../repositories/outbox.repository.js";
+
+export class DeleteProductService {
+  private logger = createLogger("DeleteProductService");
+
+  constructor(
+    private db: DrizzleDBClient,
+    private productRepository: ProductRepository,
+    private orderQueries: OrderQueries,
+    private outboxRepository: OutboxRepository,
+  ) {}
+
+  async execute(command: DeleteProductCommand): Promise<void> {
+    this.logger.info("DeleteProductService.execute called", { command });
+
+    const productId = ProductId.of(command.productId);
+
+    const product = await this.productRepository.find(productId);
+
+    if (!product) throw new NotFoundError("product", command.productId);
+
+    const existingOrderWithItemOfProduct =
+      await this.orderQueries.getFirstOrderWithItemOfProduct(productId);
+
+    if (existingOrderWithItemOfProduct) {
+      throw new ConflictError(
+        "product",
+        command.productId,
+        "there's at least one order with an item referencing this product",
+      );
+    }
+
+    const events = product.pullEvents();
+
+    this.logger.debug("Saving product", { id: product.id.value });
+
+    await this.db.transaction(async (tx) => {
+      await this.productRepository.delete(productId, tx);
+
+      if (events.length > 0) {
+        this.logger.debug("Saving events", { count: events.length });
+        await this.outboxRepository.saveEvents(events, tx);
+      }
+    });
+  }
+}
