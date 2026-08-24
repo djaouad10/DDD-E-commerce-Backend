eff359964d0ee3af4580c425382fab47ea6a24b7
unknown <djaouadgharbi10@gmail.com>
2026-08-24 14:41:00 +0100

    implemnted create-order query/service

---
+++ src/application/services/create-order-service.ts
@@ -0,0 +1,201 @@
+import { OrderItem } from "#/domain/entities/order-item.js";
+import { Order } from "#/domain/entities/order.js";
+import type { Product } from "#/domain/entities/product.js";
+import type { ShippingProviderGateway } from "#/domain/gateways/shipping-provider.gateway.js";
+import type { CartRepository } from "#/domain/repositories/cart.repository.js";
+import type { OrderRepository } from "#/domain/repositories/order.repository.js";
+import type { ProductRepository } from "#/domain/repositories/product.repository.js";
+import type { UserRepository } from "#/domain/repositories/user.repository.js";
+import type { OrderId } from "#/domain/value-objects/order-id.js";
+import { ShippingDetails } from "#/domain/value-objects/shipping-details.js";
+import { UserId } from "#/domain/value-objects/user-id.js";
+import type { VariationId } from "#/domain/value-objects/variation-id.js";
+import type { DrizzleDBClient } from "#/infrastructure/config/database.js";
+import {
+  ForbiddenError,
+  NotFoundError,
+  ValidationError,
+} from "#/shared/errors/domain-error.js";
+import { createLogger } from "#/shared/logging/logger.js";
+import type { CreateOrderCommand } from "../commands/create-order.command.js";
+import type { IdempotencyKeysRepository } from "../repositories/idempotency-keys.repository.js";
+import type { OutboxRepository } from "../repositories/outbox.repository.js";
+
+export class CreateOrderService {
+  private logger = createLogger("CreateOrderService");
+
+  constructor(
+    private db: DrizzleDBClient,
+    private orderRepository: OrderRepository,
+    private cartRepository: CartRepository,
+    private userRepository: UserRepository,
+    private shippingProviderGateway: ShippingProviderGateway,
+    private productRepository: ProductRepository,
+    private idempotencyKeysRepository: IdempotencyKeysRepository,
+    private outboxRepository: OutboxRepository,
+  ) {}
+
+  async execute(command: CreateOrderCommand): Promise<OrderId> {
+    this.logger.info(`CreateOrderService.execute called`, { command });
+
+    const {
+      userId,
+      providedShippingPrice,
+      selectedShippingProvider,
+      shippingDetails,
+      idempotencyKey,
+    } = command;
+
+    // check if the shipping price selected by client is actually the one listed by the provider (so clients aren't charged a different price than they agreed to on the frontend)
+    const [providerDeliveryPriceOfWilaya, communesOfWilaya, user, cart] =
+      await Promise.all([
+        // u would typically use an abstraction on top of shipping provider gateways to route the request based on the provider, but since I only have one provider, I'll just use the gateway directly
+        this.shippingProviderGateway.getDeliveryFeesOfWilaya(
+          shippingDetails.wilayaCode,
+        ),
+        this.shippingProviderGateway.getActiveCommunesOfWilaya(
+          shippingDetails.wilayaCode,
+        ),
+        this.userRepository.find(UserId.of(userId)),
+        this.cartRepository.findByUserId(UserId.of(userId)),
+      ]);
+
+    let providerDeliveryPrice =
+      shippingDetails.deliveryType === "TO_DESK"
+        ? providerDeliveryPriceOfWilaya.stopDeskFee
+        : providerDeliveryPriceOfWilaya.homeDeliveryFee;
+
+    // you would also include a currency check but I only use DZD here so it's not necessary
+    if (providedShippingPrice !== providerDeliveryPrice.amount) {
+      throw new ValidationError(
+        "shippingPrice",
+        "provided shipping price doesn't match the one listed by the provider",
+      );
+    }
+
+    // check if provided postal code exists on that wilaya for that shipping provider
+    const commune = communesOfWilaya.find(
+      (commune) => commune.postalCode === shippingDetails.postalCode,
+    );
+
+    if (!commune) {
+      throw new ValidationError(
+        "postalCode",
+        "provided postal code doesn't exist on that wilaya for that shipping provider",
+      );
+    }
+
+    // check if user exist
+    if (!user) {
+      throw new NotFoundError("user", `user doesn't exist with id ${userId}`);
+    }
+
+    // check if user is banned
+    if (user.isBanned()) {
+      throw new ForbiddenError("create order", userId);
+    }
+
+    if (cart.getItems().length === 0) {
+      throw new ValidationError("cart", "cart is empty");
+    }
+
+    // check if each cart item has the required variation quantity
+    const variationIds = cart.getItems().map((item) => item.variationId);
+
+    const products =
+      await this.productRepository.findByVariationIds(variationIds);
+
+    const variationToProductMap = new Map<VariationId, Product>();
+
+    for (const product of products) {
+      for (const variation of product.getVariations()) {
+        variationToProductMap.set(variation.id, product);
+      }
+    }
+
+    // for each cart item create an order item:
+    const orderItems: OrderItem[] = cart.getItems().map((item) => {
+      const targetProduct = variationToProductMap.get(item.variationId);
+
+      if (!targetProduct) {
+        throw new ValidationError(
+          "product",
+          "a product referenced by a cart item not found",
+        );
+      }
+
+      const targetVariation = targetProduct.getVariation(item.variationId);
+
+      if (!targetVariation) {
+        throw new ValidationError(
+          "variation",
+          "a variation referenced by a cart item not found",
+        );
+      }
+
+      // reserve stock
+      targetProduct.reserveStock(targetVariation.id, item.getQty());
+
+      // create the order item
+      return OrderItem.create(
+        item.variationId,
+        item.getQty(),
+        targetProduct.getPrice(),
+        targetVariation.getWeight(),
+        targetProduct.getDiscountedPrice(),
+      );
+    });
+
+    const orderShippingDetails = ShippingDetails.create(
+      shippingDetails.deliveryType,
+      shippingDetails.fullName,
+      shippingDetails.firstPhone,
+      shippingDetails.wilayaCode,
+      shippingDetails.commune,
+      shippingDetails.postalCode,
+      shippingDetails.address,
+      shippingDetails.fragile,
+      shippingDetails.secondPhone,
+      shippingDetails.gpsLink,
+      shippingDetails.clientNote,
+    );
+
+    const order = Order.create(
+      UserId.of(userId),
+      orderShippingDetails,
+      orderItems,
+      providerDeliveryPrice,
+      selectedShippingProvider,
+    );
+
+    cart.clear();
+
+    const orderEvents = order.pullEvents();
+    const productEvents = products.flatMap((p) => p.pullEvents());
+    const cartEvents = cart.pullEvents();
+
+    this.logger.debug("Saving order", { id: order.id.value });
+    await this.db.transaction(async (tx) => {
+      await this.idempotencyKeysRepository.create(
+        idempotencyKey,
+        "CreateOrderService",
+        tx,
+      );
+
+      await Promise.all([
+        this.orderRepository.save(order, tx),
+        this.cartRepository.save(cart, tx),
+        this.outboxRepository.saveEvents(
+          [...orderEvents, ...productEvents, ...cartEvents],
+          tx,
+        ),
+      ]);
+
+      await Promise.all(
+        products.map((p) => this.productRepository.save(p, tx)),
+      );
+    });
+
+    return order.id;
+  }
+}
