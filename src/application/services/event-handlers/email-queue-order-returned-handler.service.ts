783021cb8cc580660dd2e1ced4989d0a4296d642
unknown <djaouadgharbi10@gmail.com>
2026-08-27 08:58:14 +0100

    implemented email-queue-order-returned-handler command/service

---
+++ src/application/services/event-handlers/email-queue-order-returned-handler.service.ts
@@ -0,0 +1,68 @@
+import type { EmailQueueOrderReturnedHandlerCommand } from "#/application/commands/event-handlers/email-queue-order-returned-handler.command.js";
+import type { IdempotencyKeysRepository } from "#/application/repositories/idempotency-keys.repository.js";
+import type { EmailGateway } from "#/domain/gateways/email.gateway.js";
+import type { OrderRepository } from "#/domain/repositories/order.repository.js";
+import type { UserRepository } from "#/domain/repositories/user.repository.js";
+import { OrderId } from "#/domain/value-objects/order-id.js";
+import { UserId } from "#/domain/value-objects/user-id.js";
+import type { DrizzleDBClient } from "#/infrastructure/config/database.js";
+import { buildOrderReturnedEmailTemplate } from "#/infrastructure/notifications/templates/order-returned.email.template.js";
+
+import { NotFoundError } from "#/shared/errors/domain-error.js";
+import { createLogger } from "#/shared/logging/logger.js";
+
+export class EmailQueueOrderReturnedHandlerService {
+  private logger = createLogger("EmailQueueOrderReturnedHandlerService");
+
+  constructor(
+    private db: DrizzleDBClient,
+    private emailGateway: EmailGateway,
+    private userRepository: UserRepository,
+    private orderRepository: OrderRepository,
+    private idempotencyKeysRepository: IdempotencyKeysRepository,
+  ) {}
+
+  async execute(
+    command: EmailQueueOrderReturnedHandlerCommand,
+    jobId: string,
+  ): Promise<void> {
+    this.logger.info("EmailQueueOrderReturnedHandlerService.execute called");
+
+    await this.db.transaction(async (tx) => {
+      // if job was already processed this will throw unique constraint violation error
+      await this.idempotencyKeysRepository.create(
+        jobId,
+        "EmailQueueOrderReturnedHandlerService",
+        tx,
+      );
+    });
+
+    const { userId, aggregateId: orderId } = command;
+
+    const [user, order] = await Promise.all([
+      this.userRepository.find(UserId.of(userId)),
+      this.orderRepository.find(OrderId.of(orderId)),
+    ]);
+
+    if (!user) {
+      this.logger.debug("User not found", { userId });
+      throw new NotFoundError("user", userId);
+    }
+
+    if (!order) {
+      this.logger.debug("Order not found", { orderId });
+      throw new NotFoundError("order", orderId);
+    }
+
+    const orderReturnedEmailTemplate = buildOrderReturnedEmailTemplate({
+      orderSnapshot: order.toSnapshot(),
+      returnedAt: command.occurredOn,
+    });
+
+    await this.emailGateway.sendEmail(
+      user.email,
+      "Order Delivered",
+      orderReturnedEmailTemplate,
+    );
+  }
+}
