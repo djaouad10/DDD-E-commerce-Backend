bbc6fefeca37d3bbe0ce94e62a2446a987235558
unknown <djaouadgharbi10@gmail.com>
2026-08-27 09:49:55 +0100

    implemented email-queue-user-registered-handler command/service

---
+++ src/application/services/event-handlers/email-queue-user-registered-handler.service.ts
@@ -0,0 +1,55 @@
+import type { EmailQueueUserRegisteredHandlerCommand } from "#/application/commands/event-handlers/email-queue-user-registered-handler.command.js";
+import type { IdempotencyKeysRepository } from "#/application/repositories/idempotency-keys.repository.js";
+import type { EmailGateway } from "#/domain/gateways/email.gateway.js";
+import type { UserRepository } from "#/domain/repositories/user.repository.js";
+import { UserId } from "#/domain/value-objects/user-id.js";
+import type { DrizzleDBClient } from "#/infrastructure/config/database.js";
+import { buildUserRegisteredEmailTemplate } from "#/infrastructure/notifications/templates/user-registered.email.template.js";
+import { NotFoundError } from "#/shared/errors/domain-error.js";
+import { createLogger } from "#/shared/logging/logger.js";
+
+export class EmailQuUserRegisteredHanHandlerService {
+  private logger = createLogger("EmailQueueUserRegisteredHandlerService");
+
+  constructor(
+    private db: DrizzleDBClient,
+    private emailGateway: EmailGateway,
+    private userRepository: UserRepository,
+    private idempotencyKeysRepository: IdempotencyKeysRepository,
+  ) {}
+
+  async execute(
+    command: EmailQueueUserRegisteredHandlerCommand,
+    jobId: string,
+  ): Promise<void> {
+    this.logger.info("EmailQueueUserRegisteredHandlerService.execute called");
+
+    const { aggregateId: userId } = command;
+
+    const user = await this.userRepository.find(UserId.of(userId));
+
+    if (!user) {
+      this.logger.debug("User not found", { userId });
+      throw new NotFoundError("user", userId);
+    }
+
+    const userRegisteredEmailTemplate = buildUserRegisteredEmailTemplate({
+      userSnapshot: user.toSnapshot(),
+    });
+
+    await this.db.transaction(async (tx) => {
+      // if job was already processed this will throw unique constraint violation error
+      await this.idempotencyKeysRepository.create(
+        jobId,
+        "EmailQueueUserRegisteredHandlerService",
+        tx,
+      );
+
+      await this.emailGateway.sendEmail(
+        user.email,
+        "Welcome to Shop",
+        userRegisteredEmailTemplate,
+      );
+    });
+  }
+}
