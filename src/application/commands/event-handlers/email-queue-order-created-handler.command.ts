880e179f834ddea4e6c2906e42d1576c36a69734
unknown <djaouadgharbi10@gmail.com>
2026-08-27 08:43:07 +0100

    implemented email-queue-order-created-handler command/service

---
+++ src/application/commands/event-handlers/email-queue-order-created-handler.command.ts
@@ -0,0 +1,16 @@
+import type { ShippingProvider } from "#/domain/entities/order.js";
+import { DomainEventCode } from "#/domain/events/domain-event.js";
+import type { Currency } from "#/domain/value-objects/money.js";
+
+export class EmailQueueOrderCreatedHandlerCommand {
+  constructor(
+    public readonly eventType: typeof DomainEventCode.ORDER_CONFIRMED,
+    public readonly occurredOn: Date,
+    public readonly aggregateId: string,
+    public readonly userId: string,
+    public readonly itemCount: number,
+    public readonly totalPrice: number,
+    public readonly currency: Currency,
+    public readonly selectedShippingProvider: ShippingProvider,
+  ) {}
+}
