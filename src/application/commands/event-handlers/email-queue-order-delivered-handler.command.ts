31453bc4dc2cf56e7bdc96abfb051c208173fe99
unknown <djaouadgharbi10@gmail.com>
2026-08-27 08:51:42 +0100

    implemented email-queue-order-delivered-handler command/service

---
+++ src/application/commands/event-handlers/email-queue-order-delivered-handler.command.ts
@@ -0,0 +1,13 @@
+import type { ShippingProvider } from "#/domain/entities/order.js";
+import { DomainEventCode } from "#/domain/events/domain-event.js";
+
+export class EmailQueueOrderDeliveredHandlerCommand {
+  constructor(
+    public readonly eventType: typeof DomainEventCode.ORDER_CONFIRMED,
+    public readonly occurredOn: Date,
+    public readonly aggregateId: string,
+    public readonly userId: string,
+    public readonly deliveredAt: Date,
+    public readonly selectedShippingProvider: ShippingProvider,
+  ) {}
+}
