59d55a2e20da0f62d1484b38dcc8f1130ea02a1f
unknown <djaouadgharbi10@gmail.com>
2026-08-29 14:47:07 +0100

    refactored a cmd

--- src/application/commands/delete-order-from-shipping-provider.command.ts
+++ src/application/commands/delete-order-from-shipping-provider.command.ts
@@ -1,8 +1,16 @@
 import type { ShippingProvider } from "#/domain/entities/order.js";
+import { ValidationError } from "#/shared/errors/domain-error.js";
 
 export class DeleteOrderFromShippingProviderCommand {
   constructor(
     public readonly trackingNumber: string,
     public readonly shippingProvider: ShippingProvider,
-  ) {}
+  ) {
+    this.validate();
+  }
+
+  private validate() {
+    if (!this.trackingNumber)
+      throw new ValidationError("trackingNumber", "trackingNumber is required");
+  }
 }
