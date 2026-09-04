f780bbae239557e954d0a037c3667df822525f03
unknown <djaouadgharbi10@gmail.com>
2026-09-04 19:38:04 +0100

    modifed register shared infra utility to regiter both DB and DRIZZLE_DB

--- src/composition/utils/shared-registry.ts
+++ src/composition/utils/shared-registry.ts
@@ -1,14 +1,19 @@
 import type { Container } from "./container.js";
-import { DB, REDIS } from "./tokens.js";
-import { createDb } from "#/infrastructure/config/database.js";
+import { DB, DRIZZLE_DB, REDIS } from "./tokens.js";
+import { createDrizzleDB } from "#/infrastructure/config/database.js";
 
 import { createRedisConnection } from "#/infrastructure/config/redis-connection.js";
 import { env } from "#/infrastructure/config/env.js";
 
 export function registerSharedInfrastructure(container: Container): void {
   // registers shared infra singeltons: db, redis connection, queues,... etc.
-  const db = createDb({ connectionUrl: env.DATABASE_URL, maxPoolSize: 10 });
+  const db = createDrizzleDB({
+    connectionUrl: env.DATABASE_URL,
+    maxPoolSize: 10,
+  }); // in case I decided to switch to another ORM/DB I can use the new factory with DB token and keep the drizzle factory to register with DRIZZLE_DB
   container.registerInstance(DB, db);
+
+  container.registerInstance(DRIZZLE_DB, db);
 
   const redisConnection = createRedisConnection({
     host: env.REDIS_HOST,
