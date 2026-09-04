9c8f1647f5a6ecfef01c377cdb66fe0014733dc8
unknown <djaouadgharbi10@gmail.com>
2026-09-04 20:44:22 +0100

    made tx a required param in cartRepo.save/delete methods

--- src/domain/repositories/cart.repository.ts
+++ src/domain/repositories/cart.repository.ts
@@ -5,6 +5,6 @@ import type { UserId } from "../value-objects/user-id.js";
 export type CartRepository = {
   findByUserId: (userId: UserId) => Promise<Cart>; // it doesn't return Promise<Cart | null>, because if a user passed the auth check then he exists in the DB, so he either has an empty cart or a cart with items
 
-  save: (cart: Cart, tx?: TransactionClient) => Promise<void>;
-  delete: (userId: UserId, tx?: TransactionClient) => Promise<void>;
+  save: (cart: Cart, tx: TransactionClient) => Promise<void>;
+  delete: (userId: UserId, tx: TransactionClient) => Promise<void>;
 };
