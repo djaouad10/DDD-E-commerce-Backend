4d56f4873a74c38ae995674e1ac7dcbecaeda6bc
unknown <djaouadgharbi10@gmail.com>
2026-09-25 02:13:10 +0100

    add validation to semantic search query

--- src/application/queries/product-semantic-search.query.ts
+++ src/application/queries/product-semantic-search.query.ts
@@ -1,4 +1,5 @@
 import type { Color, Size } from "#/domain/entities/product.js";
+import { ValidationError } from "#/shared/errors/errors.js";
 
 export type SemanticSearchFilters = {
   colors?: Color[];
@@ -13,5 +14,40 @@ export class ProductSemanticSearchQuery {
     public readonly query: string,
     public readonly limit?: number,
     public readonly filters?: SemanticSearchFilters,
-  ) {}
+  ) {
+    this.validate();
+  }
+
+  private validate() {
+    if (this.limit && this.limit <= 0) {
+      throw new ValidationError("limit", "limit must be greater than 0");
+    }
+
+    if (this.filters?.maxPrice && this.filters?.minPrice) {
+      if (this.filters?.maxPrice <= this.filters?.minPrice) {
+        throw new ValidationError(
+          "filters.maxPrice",
+          "filters.maxPrice must be greater than filters.minPrice",
+        );
+      }
+    }
+
+    if (this.filters?.maxPrice && this.filters?.maxPrice < 0) {
+      throw new ValidationError(
+        "filters.maxPrice",
+        "filters.maxPrice must be greater than 0",
+      );
+    }
+
+    if (this.filters?.minPrice && this.filters?.minPrice < 0) {
+      throw new ValidationError(
+        "filters.minPrice",
+        "filters.minPrice must be greater or equal to 0",
+      );
+    }
+
+    if (!this.query) {
+      throw new ValidationError("query", "query is required");
+    }
+  }
 }
