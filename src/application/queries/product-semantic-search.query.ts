e9041a568a56f3b214777c4318f64807a9d59616
unknown <djaouadgharbi10@gmail.com>
2026-09-25 15:53:18 +0100

    refactored product semantic search query

--- src/application/queries/product-semantic-search.query.ts
+++ src/application/queries/product-semantic-search.query.ts
@@ -19,27 +19,29 @@ export class ProductSemanticSearchQuery {
   }
 
   private validate() {
-    if (this.limit && this.limit <= 0) {
+    if (this.limit !== undefined && this.limit <= 0) {
       throw new ValidationError("limit", "limit must be greater than 0");
     }
 
-    if (this.filters?.maxPrice && this.filters?.minPrice) {
-      if (this.filters?.maxPrice <= this.filters?.minPrice) {
-        throw new ValidationError(
-          "filters.maxPrice",
-          "filters.maxPrice must be greater than filters.minPrice",
-        );
-      }
+    if (
+      this.filters?.maxPrice !== undefined &&
+      this.filters.minPrice !== undefined &&
+      this.filters.maxPrice <= this.filters.minPrice
+    ) {
+      throw new ValidationError(
+        "filters.maxPrice",
+        "filters.maxPrice must be greater than filters.minPrice",
+      );
     }
 
-    if (this.filters?.maxPrice && this.filters?.maxPrice < 0) {
+    if (this.filters?.maxPrice !== undefined && this.filters?.maxPrice <= 0) {
       throw new ValidationError(
         "filters.maxPrice",
         "filters.maxPrice must be greater than 0",
       );
     }
 
-    if (this.filters?.minPrice && this.filters?.minPrice < 0) {
+    if (this.filters?.minPrice !== undefined && this.filters?.minPrice < 0) {
       throw new ValidationError(
         "filters.minPrice",
         "filters.minPrice must be greater or equal to 0",
